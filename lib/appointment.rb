ce1b0c1f6823e62d5f74a32245810460bf6cf535
Micah Levine <micah.levine@outlook.com>
2019-09-29 14:16:22 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,20 +1,16 @@
 class Appointment
+  attr_accessor :date, :doctor, :patient
 
+  @@all = []
 
-attr_accessor :date, :patient, :doctor
-@@all = []
+  def initialize(date, patient, doctor)
+    @date = date
+    @doctor = doctor
+    @patient = patient
+    @@all << self
+  end
 
-def initialize(patient, date, doctor)
-  @patient = patient
-  @date = date
-  @doctor = doctor
-  @@all << self
-end
-
-
-
-def self.all
-  @@all
-end
-
+  def self.all
+    @@all
+  end
 end
